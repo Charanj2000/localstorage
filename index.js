c5e1dfbb4677c4edb293611347723fb353c53a15
Charanj2000 <charanjanardhan118@gmail.com>
2022-10-15 14:36:11 +0530

    multipleuserstorage

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ function saveToLocalStorage(event){
     event.preventDefault();
     var name = event.target.username.value;
     var email= event.target.emailID.value;
-    localStorage.setItem('name' ,name);
-    localStorage.setItem('email' ,email);
+    
     const obj ={
         name,
         email,
     }
-    localStorage.setItem('userdetails' , JSON.stringify(obj));
+    localStorage.setItem(obj.email, JSON.stringify(obj));
+    showNewUserScreen(obj)
   }
-  
+ function showNewUserScreen(user){
+     const parentNode =document.getElementById('listofusers');
+     const childHTML =`<li> ${user.name} - ${user.email}</li>`
+
+     parentNode.innerHTML = parentNode.innerHTML+childHTML;
+ }
+ 
+ 
   
   
